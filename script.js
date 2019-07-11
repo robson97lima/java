11b3fd8c80bef3521aadeb4bf7bc988c8b185281
robson97lima <robson97lima@gmail.com>
2019-07-11 11:41:54 -0300

    escada

--- script.js
+++ script.js
@@ -13,19 +13,43 @@
     //alert('Que pena, você já ouviu falar da estação Hack?');}
 //else{
     //alert('Que pena, você deveria aprender JavaScript, ele é muito legal.');}
-    let nomeDoUsuario = prompt('Qual o seu nome?')
-    let Materia = prompt('Você é essa materia?')
-    let Nota = prompt('Qual a sua nota?')
-    let media = 6
+    //let nomeDoUsuario = prompt('Qual o seu nome?')
+    //let Materia = prompt('Você é essa materia?')
+    //let Nota = prompt('Qual a sua nota?')
+    //let media = 6
 
-    if (Number(Nota)>= media) {
-        alert('Parabéns!, você passou de ano')
-    }
-    else if(Number(Nota)>= media - 0.5){
-        alert('Passou pelo sistema!')
-    }
-    else {
-        alert('Você não Passou!!!')
-    }
-    Isso é um teste
+    //if (Number(Nota)>= media) {
+    //alert('Parabéns!, você passou de ano')}
+    //else if(Number(Nota)>= media - 0.5){
+        //alert('Passou pelo sistema!')}
+    //else {
+        //alert('Você não Passou!!!')}
+    //Isso é um teste
+    //let vetor =['Felipe','Carol','Lolo',false, 12, 256,-1000]
+    //let joaozinho = {
+        //nome: 'Joãozinho',
+        //sobrenome: 'Das Neves',
+        //idade: 16,
+        //pets: {
+            //nome: 'Rex',
+            //raca: 'Vira-Lata',
     
+            //nome: 'Spike',
+            //raca: 'Tomba-potes',}};
+//Para um laço precisamos de um 
+//CONTADOR;
+//CONDIÇÃO;
+
+//PASSO;
+
+//prompt('Quantos Lances de escada vc quer subir?')
+
+let lances = Number (prompt ('Quantos Lances de escada vc quer subir?'))
+let perguntaMaterial = prompt ('Qual o material da escada?')
+let material = "";
+let i = 0;
+    while (i < lances){
+        material += perguntaMaterial;
+        console.log (material);
+        i ++;
+    }
