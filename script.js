5a320aabab3c614197f6ab517eea676ddd9b5f48
robson97lima <robson97lima@gmail.com>
2019-07-11 10:00:31 -0300

    JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+//console.log('Hello Word');
+//let nomeDoUsuario = prompt('Qual o seu nome?')
+//let idade = prompt('Quantos anos você tem?')
+//let progamador = confirm('Você é um programados')
+
+//console.log('Olá ' +nomeDoUsuario);
+//alert('Ola ' + nomeDoUsuario + ', você tem ' +idade + 'anos.')
+//Console.log(idade);
+//console.log(vivo);
+//if(progamador){
+    //alert('Você é um programador, Parabéns!')}
+//else if(Number(idade)>=16 && Number(idade)<= 25){
+    //alert('Que pena, você já ouviu falar da estação Hack?');}
+//else{
+    //alert('Que pena, você deveria aprender JavaScript, ele é muito legal.');}
+    let nomeDoUsuario = prompt('Qual o seu nome?')
+    let Materia = prompt('Você é essa materia?')
+    let Nota = prompt('Qual a sua nota?')
+    let media = 6
+
+    if (Number(Nota)>= media) {
+        alert('Parabéns!, você passou de ano')
+    }
+    else if(Number(Nota)>= media - 0.5){
+        alert('Passou pelo sistema!')
+    }
+    else {
+        alert('Você não Passou!!!')
+    }
+
+    
